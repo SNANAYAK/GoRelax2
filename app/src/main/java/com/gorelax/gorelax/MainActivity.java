f2df0a7536a47cc94d8a58ba63f581371eebacef
SNANAYAK <32908320+SNANAYAK@users.noreply.github.com>
2018-09-16 02:18:42 -0400

    Added method to go to Custom page (Activity2)

--- app/src/main/java/com/gorelax/gorelax/MainActivity.java
+++ app/src/main/java/com/gorelax/gorelax/MainActivity.java
@@ -8,14 +8,26 @@ import android.widget.EditText;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewbyId(R.id.button);
+        button.setOnCLickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        }
     }
 
     public void playVid(View view) {
         Intent intent = new Intent(this, PlayVideo.class);
+        startActivity(intent);
+    }
+    public void openActivity2() {
+        Intent intent = new Intent(this,Activity2.class);
         startActivity(intent);
     }
 }
